ac18820c37ef2b83b43c77f18f12f4a7b32861ec
Zhien Zhang <startprogr_zze@aol.com>
2022-04-27 11:30:47 +1000

    Initial setup of the price predictor

---
+++ src/price_predictor/model.py
@@ -0,0 +1,14 @@
+# !/user/bin/env python
+"""
+The LSTM model to predict the price of cryptocurrencies
+"""
+
+from tensorflow import keras
+
+__author__ = "Zhien Zhang"
+__email__ = "zhien.zhang@uqconnect.edu.au"
+
+
+# starting point of the execution
+if __name__ == "__main__":
+    ...

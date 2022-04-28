a850f4b2396b56540bcf7423ca9b9daf772d8f8b
Zhien Zhang <startprogr_zze@aol.com>
2022-04-28 10:27:44 +1000

    Add the model

--- src/price_predictor/model.py
+++ src/price_predictor/model.py
@@ -11,21 +11,36 @@ __email__ = "zhien.zhang@uqconnect.edu.au"
 
 UNITS = 4
 DROPOUT = 0.2
+MODEL_PATH = "./model"
 
 
-def get_model():
-    model = keras.Sequential()
-    model.add(keras.layers.LSTM(UNITS, input_shape=(SEQ_LEN - 1, 1)))
-    model.add(keras.layers.Dropout(DROPOUT))
-    model.add(keras.layers.LSTM(UNITS))
-    model.add(keras.layers.Dropout(DROPOUT))
-    model.add(keras.layers.LSTM(UNITS))
-    model.add(keras.layers.Dropout(DROPOUT))
-    model.add(keras.layers.LSTM(UNITS))
-    model.add(keras.layers.Dense(units=5))
-    model.add(keras.layers.Activation('linear'))
+class Model:
+    def __init__(self):
+        self.layers = None
 
-    model.compile(optimizer="adagrad", loss="mean_squared_error")
+    def create_model(self):
+        model = keras.Sequential()
+        model.add(keras.layers.LSTM(UNITS, input_shape=(SEQ_LEN - 1, 1)))
+        model.add(keras.layers.Dropout(DROPOUT))
+        model.add(keras.layers.LSTM(UNITS))
+        model.add(keras.layers.Dropout(DROPOUT))
+        model.add(keras.layers.LSTM(UNITS))
+        model.add(keras.layers.Dropout(DROPOUT))
+        model.add(keras.layers.LSTM(UNITS))
+        model.add(keras.layers.Dense(units=5))
+        model.add(keras.layers.Activation('linear'))
 
-    return model
+        model.compile(optimizer="adagrad", loss="mean_squared_error")
+
+        self.layers = model
+
+    @staticmethod
+    def load_model():
+        layers = keras.models.load_model(MODEL_PATH)
+        model = Model()
+        model.layers = layers
+        return model
+
+    def save_model(self):
+        self.layers.save(MODEL_PATH)
 
--- src/price_predictor/preprocess.py
+++ src/price_predictor/preprocess.py
@@ -4,11 +4,14 @@ The pre-processing module
 """
 
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 __author__ = "Zhien Zhang"
 __email__ = "zhien.zhang@uqconnect.edu.au"
 
 SEQ_LEN = 100
+TRAIN_SPLIT = 0.8
 
 
 def to_sequences(data, seq_len):
@@ -34,4 +37,26 @@ def train_test_split(data_raw, seq_len, train_split):
 
     return X_train, y_train, X_test, y_test
 
+
+def prepare_train_data(raw_history_data: pd.DataFrame):
+    """
+    Prepare the training and testing data for the model
+
+    :param raw_history_data: a dataframe with columns: Date, Open, High, Low, Close, Adj Close, Volume
+    :return: (X_train, y_train, X_test, y_test)
+    """
+    df = raw_history_data.sort_values("Date")
+
+    # normalise the close price
+    scaler = MinMaxScaler()
+    close_price = df.Close.values.reshape(-1, 1)
+    scaled_close = scaler.fit_transform(close_price)
+
+    # remove nan rows
+    scaled_close = scaled_close[~np.isnan(scaled_close)]
+    scaled_close = scaled_close.reshape(-1, 1)
+
+    # split into train and test sets
+    X_train, y_train, X_test, y_test = train_test_split(scaled_close, SEQ_LEN, train_split=TRAIN_SPLIT)
+    return X_train, y_train, X_test, y_test
 
